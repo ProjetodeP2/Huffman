e2eb1e8eda55627946356487ea858d12c62f350a
Tarcísio Lima <tlf@ic.ufal.br>
2017-10-22 16:45:41 -0200

    Adicionado Função para recuperar o cabeçalho do arquivo comprimido

---
+++ src/get_header.c
@@ -0,0 +1,53 @@
+#include "../lib/get_header.h"
+
+void open(FILE* file)
+{
+	if (file == NULL) printf("O arquivo não pode ser aberto\n");
+	else printf("O arquivo foi aberto\n");
+}
+
+int trash(FILE* file)
+{
+	int trash_size = 0, i;
+	unsigned char aux_trash;
+
+	fscanf(file, "%c", &aux_trash);
+
+	for (i = 7; i >= 5; --i) if ((aux_trash & (1 << i))) trash_size += pow(2, (i - 5));
+
+	rewind(file);
+	return trash_size;
+}
+
+int tree_size(FILE* file)
+{
+    int size = 0, i;
+    int position_bit = 12;
+	unsigned char trash_byte1, trash_byte2;
+
+	fscanf(file, "%c%c", &trash_byte1, &trash_byte2);
+
+	for (i = 4; i >= 0 ; --i)
+	{
+		if ((trash_byte1 & (1 << i))) size += pow(2, position_bit);
+		--position_bit;
+	}
+
+	for (i = 7; i >= 0 ; --i)
+	{
+		if ((trash_byte2 & (1 << i))) size += pow(2, position_bit);
+		--position_bit;
+	}
+
+	return size;
+}
+
+unsigned char* pre_order_tree(FILE* file, int tree_size)
+{
+	int i;
+	unsigned char* tree = malloc(tree_size * sizeof(unsigned char));
+
+	for (i = 0; i < tree_size; ++i) fscanf(file, "%c", &tree[i]);
+
+	return tree;
+}
